1b86dda42f5ced1af077df4feefe15b70c013bd4
NehaS101 <solankineha297@gmail.com>
2023-07-09 22:16:58 +0530

    done with convert code

---
+++ Backend/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const { Configuration, OpenAIApi } = require('openai');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send("welcome to code convertor") })
+
+async function runprompt(mssg) {
+    try {
+        const config = new Configuration({
+            apiKey: process.env.Ai_Key
+        })
+        const openai = new OpenAIApi(config);
+        let response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: mssg,
+            max_tokens: 500,
+            temperature: 1,
+        });
+        let result = response.data.choices[0].text;
+        return result;
+    } catch (error) {
+        console.log({ error: error.message })
+    }
+}
+
+app.post('/convert', async (req, res) => {
+    let { code, language } = req.body;
+    try {
+        let response = await runprompt(`convert the ${code} into ${language}`);
+        res.send(response);
+    } catch (error) {
+        res.send({ error: error.message });
+    }
+})
+
+app.listen(process.env.port, () => {
+    console.log("listening on port " + process.env.port);
+})
